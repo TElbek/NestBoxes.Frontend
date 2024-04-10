cfbb9ca592c58cff5777b8f19ccf2d9dee2fb145
Thomas Elbek <vadholm@hotmail.com>
2024-04-10 15:29:08 +0200

    Login form

--- src/components/main/login.vue
+++ src/components/main/login.vue
@@ -1,72 +1,68 @@
 <template>
     <div class="center-container">
-        <form @submit.prevent="login" method="post">
-            <div>
-                <input type="text" placeholder="brugernavn" name="uname" required v-model="state.username" ref="username" v-focus/>
-                <input type="password" placeholder="kodeord" name="pwd" required v-model="state.password" ref="pwd"/>
-                <button type="submit">Login</button>
+        <form class="border rounded shadow-sm">
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Brugernavn" name="uname" v-model="state.username"
+                    ref="username" v-focus />
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" placeholder="Kodeord" name="pwd" v-model="state.password"
+                    ref="pwd" />
+            </div>
+            <div class="btn-grid mb-3">
+                <button class="btn btn-sm btn-success" :disabled="!canLogin" @click="login">Login</button>
+                <button class="btn btn-sm btn-dark" @click="goBack">Fortryd</button>
             </div>
         </form>
     </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 
 const state = reactive({
     username: '',
     password: ''
- });
+});
+
+const canLogin = computed(() => {
+    return state.username.length > 0 &&
+        state.password.length > 0;
+});
 
 function login() {
+
+}
+
+function goBack() {
 }
 </script>
 
 <style scoped>
-    form {
-        padding: 10px;
-    }
+form {
+    padding: 15px;
+    margin: 5px;
+}
 
-    form {
-        border: 1px solid #efefef;
-        border-radius: 3px;
-    }
+.btn-grid {
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    gap: 20px;
+}
 
-    input[type="text"],
-    input[type="password"] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0px;
-        display: inline-block;
-        border: 1px solid #efefef;
-        box-sizing: border-box;
-    }
+.btn {
+    width: 100%;
+}
 
-    button {
-        background-color: #afafaf;
-        color: black;
-        padding: 14px 20px;
-        max-width: 8px 0px;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-        border-radius: 3px;
-    }
-
-    button:hover {
-        opacity: 0.8;
-        font-weight: bold;
-    }
-
-    .center-container {
-        width: 270px;
-        height: 40%;
-        overflow: auto;
-        margin: auto;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }
+.center-container {
+    width: 250px;
+    height: 40%;
+    overflow: auto;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
 </style>
