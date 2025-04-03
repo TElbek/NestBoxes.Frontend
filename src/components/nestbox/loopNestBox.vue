6862d50691597a6fdcba82ff1bde8214d1caa6d9
Thomas Elbek <vadholm@hotmail.com>
2025-04-03 09:26:24 +0200

    Vi besked hvis ingen redekasser i liste.

--- src/components/nestbox/loopNestBox.vue
+++ src/components/nestbox/loopNestBox.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="mb-3 scroll">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 g-2">
+        <div v-if="props.nestBoxList.length > 0" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5 g-2">
             <div v-for="box in nestBoxList" class="col">
                 <nestbox :nestbox="box" :showBrief="showBrief"></nestbox>
             </div>
+        </div>
+        <div v-else>
+            <span class="h5">Ingen Redekasser</span>
         </div>
     </div>
 </template>
