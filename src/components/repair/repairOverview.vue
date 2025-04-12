a1957e43044ae03984af9da8c8748e8002283916
Thomas Elbek <vadholm@hotmail.com>
2025-04-12 20:06:41 +0200

    repairOverview - minor UI improvements

--- src/components/repair/repairOverview.vue
+++ src/components/repair/repairOverview.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="row">
-        <span class="col h4">Reparation</span>
+    <div class="row border-bottom">
+        <span class="col h4">{{ route.meta.title }}</span>
         <div class="col-auto">
             <div class="btn btn-sm btn-success" @click="addRepair()">Opret</div>
         </div>
     </div>
-    <div class="border-top scroll mb-2">
+    <div class="scroll mb-2">
         <div v-if="state.hasData" class="mb-2 row row-cols-1 row-cols-lg-3 g-3">
             <div v-for="repairTypeId in uniqueRepairTypes">
-                <div class="fw-bold mb-1 mt-2 h5 pb-1">
+                <div class="fw-bold mb-1 mt-2 h5">
                     <span>{{ getRepairTypeNameById(repairTypeId) }}</span>
                     <span class="ms-1">({{ countOfReparsForRepairType(repairTypeId) }})</span>
                 </div>
@@ -27,8 +27,9 @@ import api from '@/api';
 import { onMounted, reactive, computed, watch } from 'vue';
 import repair from '@/components/repair/repair.vue';
 
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 const router = useRouter();
+const route = useRoute();
 
 const state = reactive({
     repairList: {},
